99d80d9088077bd5ff4ff4c01ec40627476840d8
sean.rogers <sean.rogers@CHI-L-SROGE>
2023-08-25 01:57:08 -0500

    before changing descendants

--- Rooted-Tree/Rooted-Tree/Program.cs
+++ Rooted-Tree/Rooted-Tree/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Xml.Linq;
 
 class TreeNode<T>
 {
@@ -15,7 +16,7 @@ class TreeNode<T>
     {
         NodeNumber = nodeNumber;
         Value = value;
-        Descendants = new List<TreeNode<T>>();
+        //Descendants = new List<TreeNode<T>>();
         Descendants.Add(this);
     }
 
@@ -114,21 +115,76 @@ class RootedTree<T>
 
 class Solution
 {
+    static Dictionary<int, TreeNode<int>> GetNodeList(int rootNumber, int numNodes)
+    {
+        Dictionary<int, TreeNode<int>> nodes = new Dictionary<int, TreeNode<int>>();
+        for (int i = 1; i <= numNodes; i++)
+        {
+            if (i != rootNumber)
+            {
+                nodes.Add(i, new TreeNode<int>(i));
+            }
+        }
+        return nodes;
+    }
+    static RootedTree<int> CreateTree(Dictionary<int, TreeNode<int>> nodes,int[][] edges, int rootNumber)
+    {
+        
+        RootedTree<int> tree = new RootedTree<int>(rootNumber);
+
+        
+        bool beforeRoot = true;
+        for (int i =0; i <edges.Length; i++)
+        {
+           
+            int firstNum = edges[i][0];
+            int secondNum = edges[i][1];
+            // -1 and -2 are for indexing of List<TreeNode<int>> nodes relative to TreeNode<int>(i)
+            if (firstNum == rootNumber)
+            {
+                tree.Root.AddChild(nodes[secondNum]);
+                beforeRoot = false;
+            }
+            else if ( secondNum == rootNumber )
+            {
+                tree.Root.AddChild(nodes[firstNum]);
+                beforeRoot = false;
+            }
+            
+            else if (!beforeRoot)
+            {
+                TreeNode<int> currNode = nodes[firstNum];
+                currNode.AddChild(nodes[secondNum]);
+            }
+            else if (beforeRoot)
+            {
+                nodes[secondNum].AddChild(nodes[firstNum]);
+            }
+
+        }
+        return tree;
+    }
     static void Main(String[] args)
     {
         string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
         int numNodes = Convert.ToInt32(firstMultipleInput[0]);
         int numQueries = Convert.ToInt32(firstMultipleInput[1]);
         int rootNumber = Convert.ToInt32(firstMultipleInput[2]);
+
+        //Create Tree
         int[][] edges = new int[numNodes-1][];
         for (int i = 0; i < numNodes-1; i++)
         {
             string[] edgeInput = Console.ReadLine().TrimEnd().Split(' ');
             int node1 = Convert.ToInt32(edgeInput[0]);
             int node2 = Convert.ToInt32(edgeInput[1]);
-            int[] edge = new int[2] { node1, node2 };
+            int[] edge = new int[2] {node1, node2};
             edges[i]=edge;
         }
+
+        Dictionary<int, TreeNode<int>> nodes = GetNodeList(rootNumber, numNodes);
+        RootedTree<int> tree = CreateTree(nodes,edges, rootNumber);
+        
         int[][] operations = new int[numQueries][];
         for(int i = 0; i < numQueries; i++)
         {
@@ -147,19 +203,20 @@ class Solution
                 operations[i] = new int[2] { A, B};
             }
         }
-        List<TreeNode<int>> nodes = new List<TreeNode<int>>();
+        /*List<TreeNode<int>> nodes = new List<TreeNode<int>>();
 
-        for (int i = 2; i <=numNodes; i++)
+        for (int i = 1; i <=numNodes; i++)
         {
-            nodes.Add(new TreeNode<int>(i));
-        }
-
-        RootedTree<int> tree = new RootedTree<int>(rootNumber);
-        foreach (int[] edge in edges)
+            if (i != rootNumber)
+            {
+                nodes.Add(new TreeNode<int>(i));
+            }
+        }*/
+        /*foreach (int[] edge in edges)
         {
             int parentNum = edge[0];
             int childNum = edge[1];
-            if (parentNum == 1)
+            if (parentNum == rootNumber)
             {
                 tree.Root.AddChild(nodes[childNum - 2]);
             }
@@ -167,16 +224,20 @@ class Solution
             {
                 nodes[parentNum - 2].AddChild(nodes[childNum - 2]);
             }
-        }
-        for(int i = 0; i< operations.Length; i++) 
+        }*/
+        
+        
+        for (int i = 0; i< operations.Length; i++) 
         {
             if (operations[i].Length==3) 
             {
                 int T = operations[i][0];
                 int V = operations[i][1];
                 int K = operations[i][2];
+
                 
-                List<TreeNode<int>> desc = nodes[T-2].Descendants;
+
+                List<TreeNode<int>> desc = nodes[T].Descendants;
                 for (int j = 0; j < desc.Count; j++)
                 {
                     int d = desc[0].DistanceTo(desc[j]);
@@ -187,14 +248,15 @@ class Solution
             { 
                 int A = operations[i][0];
                 int B = operations[i][1];
+                
                 List<TreeNode<int>> path = new List<TreeNode<int>>();
                 if (A == rootNumber)
                 {
-                    path = tree.Root.PathTo(nodes[B - 2]);
+                    path = tree.Root.PathTo(nodes[B]);
                 }
                 else
                 {
-                    path = nodes[A-2].PathTo(nodes[B - 2]);
+                    path = nodes[A].PathTo(nodes[B]);
                 }
                 int value = 0;
                 int mod = Convert.ToInt32(Math.Pow(10,9) + 7);
