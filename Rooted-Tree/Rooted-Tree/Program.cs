5c289d97d3ccd5f4c1395dfe009a8df826a4fe2a
Sean Rogers <rogers.sean.tyler@gmail.com>
2023-08-30 12:42:46 -0500

    used binary lifting for finding LCA

--- Rooted-Tree/Rooted-Tree/Program.cs
+++ Rooted-Tree/Rooted-Tree/Program.cs
@@ -3,131 +3,126 @@ using System.Collections.Generic;
 using System.IO;
 using System.Xml.Linq;
 
-class TreeNode<T>
+class TreeNode
 {
-    public T NodeNumber { get; }
+    public int NodeNumber { get; }
     public int Value { get; set; }
-    public TreeNode<T> Parent { get; set; }
-    public List<TreeNode<T>> Children { get; } = new List<TreeNode<T>>();
+    public TreeNode Parent { get; set; }
+    public List<TreeNode> Children { get; } = new List<TreeNode>();
 
-    public List<TreeNode<T>> Descendants { get; } = new List<TreeNode<T>>();
+    public List<TreeNode> Descendants { get; } = new List<TreeNode>();
 
-    public TreeNode(T nodeNumber, int value = 0)
+    public TreeNode(int nodeNumber, int value = 0)
     {
         NodeNumber = nodeNumber;
         Value = value;
-        //Descendants = new List<TreeNode<T>>();
-        Descendants.Add(this);
-    }
-
-    public void AddChild(TreeNode<T> child)
-    {
-        child.Parent = this;
-        Children.Add(child);
-        Descendants.AddRange(child.Descendants);
-        TreeNode<T> dummyParent = Parent; 
-        while (dummyParent != null)
-        {
-            dummyParent.Descendants.AddRange(child.Descendants);
-            dummyParent = dummyParent.Parent;
-        }
         
     }
 
-    public int DistanceTo(TreeNode<T> target)
+    public void AddChild(TreeNode child)
     {
-        var path = PathTo(target);
-        if (path == null)
-            return -1;
-        return path.Count - 1; // Subtract 1 because the path includes the start node
+        child.Parent = this;
+        Children.Add(child);
+        
+        
     }
-
-    public List<TreeNode<T>> PathTo(TreeNode<T> target)
-    {
-        return PathTo(target, new HashSet<TreeNode<T>>());
-    }
-
-    private List<TreeNode<T>> PathTo(TreeNode<T> target, HashSet<TreeNode<T>> visitedNodes)
-    {
-        if (this == target) return new List<TreeNode<T>>() { this };
-
-        visitedNodes.Add(this);
-
-        // Search descendants.
-        foreach (var child in Children)
-        {
-            if (!visitedNodes.Contains(child))
-            {
-                var path = child.PathTo(target, visitedNodes);
-                if (path != null)
-                {
-                    path.Insert(0, this);
-                    return path;
-                }
-            }
-        }
-
-        // Search ancestors.
-        if (this.Parent != null && !visitedNodes.Contains(this.Parent))
-        {
-            var pathToParent = this.Parent.PathTo(target, visitedNodes);
-            if (pathToParent != null)
-            {
-                pathToParent.Add(this);
-                return pathToParent;
-            }
-        }
-
-        return null;
-    }
-
-
-    public IEnumerable<TreeNode<T>> GetDescendants()
-    {
-        yield return this;
-        foreach (var child in Children)
-        {
-            foreach (var descendant in child.GetDescendants())
-            {
-                yield return descendant;
-            }
-        }
-    }
+       
 }
 
-class RootedTree<T>
+class RootedTree
 {
-    public TreeNode<T> Root { get; private set; }
-
-    public RootedTree(T rootNumber, int rootValue = 0)
+    public TreeNode Root { get; private set; }
+    private int[,] up;  // For binary lifting
+    private int[] depth;  // Store the depth of each node
+    private int maxLog;  // Maximum value of log2(N)
+    public RootedTree(int rootNumber,int nodeCount, int rootValue = 0)
     {
-        Root = new TreeNode<T>(rootNumber, rootValue);
+        Root = new TreeNode(rootNumber, rootValue);
+        maxLog = (int)Math.Ceiling(Math.Log(nodeCount, 2));
+        up = new int[nodeCount + 1, maxLog + 1];  // Assuming 1-based node numbering
+        depth = new int[nodeCount + 1];
+        Precompute(rootNumber, Root, null);
+    }
+    public void Precompute(int nodeNumber, TreeNode node, TreeNode parent)
+    {
+        up[nodeNumber, 0] = parent?.NodeNumber ?? -1;  // Initialize the parent
+        depth[nodeNumber] = parent == null ? 0 : depth[parent.NodeNumber] + 1;
+
+        // Precompute up[][] for this node
+        for (int i = 1; i <= maxLog; i++)
+        {
+            int ancestor = up[nodeNumber, i - 1];
+            up[nodeNumber, i] = ancestor == -1 ? -1 : up[ancestor, i - 1];
+        }
+
+        // Precompute for children
+        foreach (var child in node.Children)
+        {
+            Precompute(child.NodeNumber, child, node);
+        }
+    }
+    public int Depth(TreeNode node)
+    {
+        int depth = 0;
+        TreeNode dummyParent = node;
+        while (dummyParent != Root)
+        {
+            depth++;
+            dummyParent = dummyParent.Parent;
+        }
+        return depth;
+    }
+    public int FindLCA(int u, int v)
+    {
+        if (depth[u] < depth[v]) (u, v) = (v, u);  // Ensure u is deeper
+
+        int diff = depth[u] - depth[v];
+        // Lift u to the same depth as v
+        for (int i = 0; i <= maxLog; i++)
+        {
+            if ((diff & (1 << i)) != 0)
+                //decomposies diff into powers of 2 and checks which ones aren't 0.
+            {
+                u = up[u, i];
+            }
+        }
+
+        if (u == v) return u;  // v was an ancestor of u
+
+        // Lift both u and v towards the root
+        for (int i = maxLog; i >= 0; i--)
+        {
+            if (up[u, i] != up[v, i])
+            {
+                u = up[u, i];
+                v = up[v, i];
+            }
+        }
+
+        return up[u, 0];  // or up[v, 0]
     }
 
-    public IEnumerable<TreeNode<T>> GetAllDescendants()
-    {
-        return Root.GetDescendants();
-    }
+
 }
 
 class Solution
 {
-    static Dictionary<int, TreeNode<int>> GetNodeList(int rootNumber, int numNodes)
+    static Dictionary<int, TreeNode> GetNodeList(int rootNumber, int numNodes)
     {
-        Dictionary<int, TreeNode<int>> nodes = new Dictionary<int, TreeNode<int>>();
+        Dictionary<int, TreeNode> nodes = new Dictionary<int, TreeNode>();
         for (int i = 1; i <= numNodes; i++)
         {
             if (i != rootNumber)
             {
-                nodes.Add(i, new TreeNode<int>(i));
+                nodes.Add(i, new TreeNode(i));
             }
         }
         return nodes;
     }
-    static RootedTree<int> CreateTree(Dictionary<int, TreeNode<int>> nodes,int[][] edges, int rootNumber)
+    static RootedTree CreateTree(Dictionary<int, TreeNode> nodes,int[][] edges, int rootNumber,int nodeCount)
     {
         
-        RootedTree<int> tree = new RootedTree<int>(rootNumber);
+        RootedTree tree = new RootedTree(rootNumber, nodeCount);
 
         
         bool beforeRoot = true;
@@ -136,7 +131,7 @@ class Solution
            
             int firstNum = edges[i][0];
             int secondNum = edges[i][1];
-            // -1 and -2 are for indexing of List<TreeNode<int>> nodes relative to TreeNode<int>(i)
+            // -1 and -2 are for indexing of List<TreeNode> nodes relative to TreeNode(i)
             if (firstNum == rootNumber)
             {
                 tree.Root.AddChild(nodes[secondNum]);
@@ -150,7 +145,7 @@ class Solution
             
             else if (!beforeRoot)
             {
-                TreeNode<int> currNode = nodes[firstNum];
+                TreeNode currNode = nodes[firstNum];
                 currNode.AddChild(nodes[secondNum]);
             }
             else if (beforeRoot)
@@ -160,6 +155,14 @@ class Solution
 
         }
         return tree;
+    }
+    static void Update(RootedTree tree, int U, int V, int K)
+    {
+        
+    }
+    static void Query(RootedTree tree, int A, int B)
+    {
+
     }
     static void Main(String[] args)
     {
@@ -179,9 +182,9 @@ class Solution
             edges[i]=edge;
         }
 
-        Dictionary<int, TreeNode<int>> nodes = GetNodeList(rootNumber, numNodes);
-        RootedTree<int> tree = CreateTree(nodes,edges, rootNumber);
-        
+        Dictionary<int, TreeNode> nodes = GetNodeList(rootNumber, numNodes);
+        RootedTree tree = CreateTree(nodes,edges, rootNumber, numNodes);
+        tree.Precompute(rootNumber, tree.Root, null);
         int[][] operations = new int[numQueries][];
         for(int i = 0; i < numQueries; i++)
         {
@@ -200,69 +203,25 @@ class Solution
                 operations[i] = new int[2] { A, B};
             }
         }
-        /*List<TreeNode<int>> nodes = new List<TreeNode<int>>();
 
-        for (int i = 1; i <=numNodes; i++)
+
+        for (int i = 0; i < operations.Length; i++)
         {
-            if (i != rootNumber)
-            {
-                nodes.Add(new TreeNode<int>(i));
-            }
-        }*/
-        /*foreach (int[] edge in edges)
-        {
-            int parentNum = edge[0];
-            int childNum = edge[1];
-            if (parentNum == rootNumber)
-            {
-                tree.Root.AddChild(nodes[childNum - 2]);
-            }
-            else
-            {
-                nodes[parentNum - 2].AddChild(nodes[childNum - 2]);
-            }
-        }*/
-        
-        
-        for (int i = 0; i< operations.Length; i++) 
-        {
-            if (operations[i].Length==3) 
+            if (operations[i].Length == 3)
             {
                 int T = operations[i][0];
                 int V = operations[i][1];
                 int K = operations[i][2];
+                Update(tree, T, V, K);
 
-                
 
-                List<TreeNode<int>> desc = nodes[T].Descendants;
-                for (int j = 0; j < desc.Count; j++)
-                {
-                    int d = desc[0].DistanceTo(desc[j]);
-                    desc[j].Value += (V + d * K);
-                }
+
             }
-            if (operations[i].Length==2) 
-            { 
+            if (operations[i].Length == 2)
+            {
                 int A = operations[i][0];
                 int B = operations[i][1];
-                
-                List<TreeNode<int>> path = new List<TreeNode<int>>();
-                if (A == rootNumber)
-                {
-                    path = tree.Root.PathTo(nodes[B]);
-                }
-                else
-                {
-                    path = nodes[A].PathTo(nodes[B]);
-                }
-                int value = 0;
-                int mod = Convert.ToInt32(Math.Pow(10,9) + 7);
-                foreach(TreeNode<int> node in path)
-                {
-                    value += node.Value % mod;
-                }
-                Console.WriteLine(value);
-
+                Query(tree, A, B);
             }
         }
         Console.ReadLine();
